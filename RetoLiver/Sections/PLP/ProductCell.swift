318c17e595e764c19f83e982a6586f5dae3de775
Alejandro Hernández <alejhernandezvaz@hotmail.com>
2020-03-18 02:21:58 -0600

    Controlador de PLP
    AlexH: Creación de clase de controlador de PLP
    AlexH: Creación de clase de celda de producto
    AlexH: Se añadieron los protocolos y delegados del grid de productos

---
+++ RetoLiver/Sections/PLP/ProductCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProductCell.swift
+//  RetoLiver
+//
+//  Created by AlejandroH on 18/03/20.
+//  Copyright © 2020 AlejandroH. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+}
