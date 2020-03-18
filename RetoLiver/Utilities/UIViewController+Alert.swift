f74d682b28d540f1f6c6de1f93e39181ca53c671
Alejandro Hernández <alejhernandezvaz@hotmail.com>
2020-03-18 04:55:32 -0600

    Create UIViewController+Alert.swift
    AlexH: Se añade alerta de error

---
+++ RetoLiver/Utilities/UIViewController+Alert.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController+Alert.swift
+//  RetoLiver
+//
+//  Created by AlejandroH on 18/03/20.
+//  Copyright © 2020 AlejandroH. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func showMessage(_ message: String, actionTitle: String, completion: (() -> ())? = nil) {
+        let alert: UIAlertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: actionTitle, style: .default, handler: { (_) in
+            completion?()
+        }))
+        self.presentViewController(alert, animated: true)
+    }
+    
+}
